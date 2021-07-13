4e6cbc40a58f51c11241d25196c47e7e7b120f42
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-13 16:05:47 -0300

    Adicionar arquivos de projeto.

---
+++ 10.Manual.Abstract.02/Entities/Enums/Color.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities.Enums {
+    enum Color {
+        Black,
+        Blue,
+        Red
+    }
+}
---
+++ 10.Manual.Abstract.02/Entities/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Enums;
+namespace Entities {
+    abstract class Shape {
+        public Color Color { get; set; }
+        protected Shape(Color color) {
+            Color = color;
+        }
+        public abstract double Area();
+    }
+}
